77fa3653636adadf081a00fd8d859353b193dbe9
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-02-16 12:34:09 -0500

    added components and pages to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header/Header'
+import About from './pages/About/About'
+import Projects from './pages/Projects/Projects'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
 
   return (
     <div>
-      Hello World!
+      <Header />
+      <About />
+      <Projects />
+      <Footer />
     </div>
   )
 }
