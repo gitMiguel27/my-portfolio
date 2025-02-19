ca0955a33f1d3ed54eb889741a37c84f2dc1dd27
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-02-19 12:31:38 -0500

    removed react scroll effect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,16 @@
 import { Container, Nav, Navbar } from "react-bootstrap"
-import {Link as Scroll} from 'react-scroll'
 
 function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark" sticky="top" style={{ height: '50px' }}>
       <Container>
-        <Navbar.Brand as={Nav.Link}>
-          <Scroll to="hero" smooth={true} offset={-100} duration={350}>Miguel Nazario</Scroll>
-        </Navbar.Brand>
+        <Navbar.Brand href="#hero">Miguel Nazario</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link>
-              <Scroll to="hero" smooth={true} offset={-100} duration={350}>Hero</Scroll>
-            </Nav.Link>
-            <Nav.Link>
-              <Scroll to="about" smooth={true} offset={-100} duration={350}>About</Scroll>
-            </Nav.Link>
-            <Nav.Link>
-              <Scroll to="projects" smooth={true} offset={-100} duration={350}>Projects</Scroll>
-            </Nav.Link>
+            <Nav.Link href="#hero">Hero</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
