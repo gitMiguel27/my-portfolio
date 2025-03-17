36aed4ce2ff4a7d1222a4bea27762a957ff26604
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-03-17 15:24:19 -0400

    added motion to hero

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,13 +1,26 @@
 import { Col, Container, Image, Row } from "react-bootstrap"
+import { motion } from "motion/react"
 
 function Hero() {
   return (
     <Container id='home' className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh', scrollSnapAlign: 'center' }} >
       <Row>
         <Col md={7} >
-          <h3>Hey, there!</h3>
-          <h1>I&apos;m Miguel Nazario<br/> a Curious and Creative Software Engineer</h1>
-          <h3>passionate in building user experiences<br/> with a focus on accessibility.</h3>
+          <motion.h3 
+            initial={{ opacity: 0, x: -500 }}
+            transition={{ type: "spring", stiffness: 100, damping: 30 }}
+            whileInView={{ opacity: 1, x:0 }}
+            >Hey, there!</motion.h3>
+          <motion.h1 
+            initial={{ opacity: 0, x: -500 }}
+            transition={{ type: "spring", stiffness: 100, damping: 25 }}
+            whileInView={{ opacity: 1, x:0 }}
+            >I&apos;m Miguel Nazario<br/> a Curious and Creative Software Engineer</motion.h1>
+          <motion.h3 
+            initial={{ opacity: 0, x: -500 }}
+            transition={{ type: "spring", stiffness: 100, damping: 20 }}
+            whileInView={{ opacity: 1, x:0 }}
+            >passionate in building user experiences<br/> with a focus on accessibility.</motion.h3>
         </Col>
         <Col md={5} >
           <Image src="/profile.png" alt="profile pic" rounded style={{ height: '50vh' }} />
