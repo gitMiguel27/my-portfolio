7daaba2a455a54d42e40777d782f625f967d9d27
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-03-17 17:15:17 -0400

    fixed responsive motion for hero and about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,30 +1,50 @@
 import { Col, Container, Row } from "react-bootstrap"
 import { motion } from "motion/react"
+import { useMediaQuery } from "@mui/material";
 
 function About() {
+  const isDesktop = useMediaQuery('(min-width:600px)')
+
+  const variants = isDesktop
+  ? {
+    initial: { opacity: 0, x: -500 },
+    transition: { type: "spring", stiffness: 100, damping: 30 },
+    whileInView: { opacity: 1, x:0 }
+  }
+  : {
+    initial: { opacity: 0 },
+    transition: { duration: 2 },
+    whileInView: { opacity: 1 }
+  }
+
   return (
     <Container id='about' className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh', scrollSnapAlign: 'center' }} >
         <Row>
           <motion.h1
-            initial={{ opacity: 0, x: -500 }}
-            transition={{ type: "spring", stiffness: 100, damping: 30 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            variants={variants}
+            initial={variants.initial}
+            transition={variants.transition}
+            whileInView={variants.whileInView}
+            className="mt-5 pt-3"
           >This is me...</motion.h1>
           <Col md={12} lg={8} >
           <motion.p
-            initial={{ opacity: 0, x: -500 }}
-            transition={{ type: "spring", stiffness: 100, damping: 30 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            variants={variants}
+            initial={variants.initial}
+            transition={variants.transition}
+            whileInView={variants.whileInView}
           >I am passionate in working on projects that value accessibility. Accessibility to me could mean anything from streamlining information that is otherwise difficult to find or understand all the way to building websites in multiple languages to reach a wider audience. Whether it&apos;s a website that shares recipes in English and Spanish, a guide to building a skincare routine, or a game that encourages people to visit underrated parts of NYC, I love thinking about how my projects will make an impact beyond its surface level.</motion.p>
           <motion.p
-            initial={{ opacity: 0, x: -500 }}
-            transition={{ type: "spring", stiffness: 100, damping: 30 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            variants={variants}
+            initial={variants.initial}
+            transition={variants.transition}
+            whileInView={variants.whileInView}
           >I bring a unique perspective that values curiosity. My background in Philosophy allows me to not only analyze dense amounts of information at a time, but also develop thoughtful solutions with a wholistic approach. It has also helped me think logicially about the code I write and how I can get from point A to point B in an efficient manner while problem-solving along the way.</motion.p>
           <motion.p
-            initial={{ opacity: 0, x: -500 }}
-            transition={{ type: "spring", stiffness: 100, damping: 30 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            variants={variants}
+            initial={variants.initial}
+            transition={variants.transition}
+            whileInView={variants.whileInView}
           >I also bring a creative side to programming. I possess entrenpreneurial qualities in terms of thinking about innovative ways to solve new and existing problems. I am passionate about photography, fashion, and design. This explains my minimalistic approach to the user interfaces I create and gravitate to.</motion.p>
           </Col>
           <Col md={12} lg={4} >
--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,25 +1,43 @@
 import { Col, Container, Image, Row } from "react-bootstrap"
 import { motion } from "motion/react"
+import { useMediaQuery } from "@mui/material";
 
 function Hero() {
+  const isDesktop = useMediaQuery('(min-width:600px)')
+
+  const variants = isDesktop
+  ? {
+    initial: { opacity: 0, x: -500 },
+    transition: { type: "spring", stiffness: 100, damping: 30 },
+    whileInView: { opacity: 1, x:0 }
+  }
+  : {
+    initial: { opacity: 0 },
+    transition: { duration: 2 },
+    whileInView: { opacity: 1 }
+  }
+
   return (
     <Container id='home' className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh', scrollSnapAlign: 'center' }} >
       <Row>
         <Col md={7} >
           <motion.h3 
-            initial={{ opacity: 0, x: -500 }}
-            transition={{ type: "spring", stiffness: 100, damping: 30 }}
-            whileInView={{ opacity: 1, x:0 }}
+            variants={variants}
+            initial={variants.initial}
+            transition={variants.transition}
+            whileInView={variants.whileInView}
           >Hey, there!</motion.h3>
           <motion.h1 
-            initial={{ opacity: 0, x: -500 }}
-            transition={{ type: "spring", stiffness: 100, damping: 25 }}
-            whileInView={{ opacity: 1, x:0 }}
+            variants={variants}
+            initial={variants.initial}
+            transition={variants.transition}
+            whileInView={variants.whileInView}
           >I&apos;m Miguel Nazario<br/> a Curious and Creative Software Engineer</motion.h1>
           <motion.h3 
-            initial={{ opacity: 0, x: -500 }}
-            transition={{ type: "spring", stiffness: 100, damping: 20 }}
-            whileInView={{ opacity: 1, x:0 }}
+            variants={variants}
+            initial={variants.initial}
+            transition={variants.transition}
+            whileInView={variants.whileInView}
           >passionate in building user experiences<br/> with a focus on accessibility.</motion.h3>
         </Col>
         <Col md={5} >
